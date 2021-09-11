1728dad86b30c0454e11a899c478d3a25e8fda46
Valyx <valyx26@gmail.com>
2021-09-11 09:54:49 +0000

    Create main function for the game to run in and function for the choice options

---
+++ assets/javascript/script.js
@@ -0,0 +1,19 @@
+const game = () => {
+	let playerScore = 0;
+	let computerScore = 0;
+	let moves = 0;
+
+	/**Function to alocate the available moves */
+	const playGame = () => {
+    const rockBtn = document.querySelector('.rock');
+    const paperBtn = document.querySelector('.paper');
+    const scissorBtn = document.querySelector('.scissor');
+    const lizzardBtn = document.querySelector('.lizzard');
+    const spockBtn = document.querySelector('.spock');
+    const playerOptions = [rockBtn, paperBtn, scissorBtn, lizzardBtn, spockBtn];
+    const computerOptions = ['fas fa-hand-rock', 'fas fa-hand-paper', 'fas fa-hand-scissors', 'fas fa-hand-lizard', 'fas fa-hand-spock'];
+    }
+
+
+
+}
